af158782f55d79af9a10fb3472e14372181c26d4
imprajwal <atharvak3.2000@gmail.com>
2026-07-01 23:22:33 +0530

    updated version for number to binary equivalent

--- AssignmentNo13/Question4.py
+++ AssignmentNo13/Question4.py
@@ -1,12 +1,26 @@
 """
-Write a program which accept one number and print binary equivalent
+Write a program which accepts one number and prints its binary equivalent.
 """
-def displayBinary(no):
-          pass
-def main():
-          no = int(input("Enter a length: "))
 
-          displayBinary(no)
-          
+def displayBinary(no):
+
+    if no == 0:
+        print(0)
+        return
+
+    binary = ""
+
+    while no > 0:
+        binary = chr((no % 2) + 48) + binary
+        no //= 2
+
+    print(binary)
+
+
+def main():
+    no = int(input("Enter a number: "))
+    displayBinary(no)
+
+
 if __name__ == "__main__":
-          main()
+    main()
