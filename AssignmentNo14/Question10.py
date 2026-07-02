49b8e4f3732e6905ebdc20e71e1dc163766543b3
imprajwal <atharvak3.2000@gmail.com>
2026-07-02 16:57:18 +0530

    lambda function logic maximum of three numbers

--- AssignmentNo14/Question10.py
+++ AssignmentNo14/Question10.py
@@ -1,5 +1,17 @@
 """
 Write a lambda function which accept three numbers from user and return largest number
-Input : 11 21 51
+Input : 21 11 51
 Outpur : 51
 """
+
+MaxThreeNumX = lambda no1,no2,no3 : no1 if (no1 >= no2 and no1 >= no3) else (no2 if no2 >= no3 else no3)                                        
+
+def main():
+          no1 = int(input("Enter a first number :"))
+          no2 = int(input("Enter a second number :"))
+          no3 = int(input("Enter a third number :"))
+
+          ret = MaxThreeNumX(no1,no2,no3)
+          print("Multiplication of  number :",ret)
+if __name__=="__main__":
+          main()
